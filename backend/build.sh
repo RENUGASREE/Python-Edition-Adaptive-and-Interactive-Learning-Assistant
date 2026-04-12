d5e893db0432671871f972f7074873c7bcab18a1
Project Owner <user@example.com>
2026-04-12 19:30:34 +0530

    Automate superuser creation in build script

--- backend/build.sh
+++ backend/build.sh
@@ -7,6 +7,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py ensure_superuser --noinput
 python manage.py seed_platform_data
 python manage.py seed_curriculum_data
 python manage.py seed_placement_quiz
